8366e38fa61a136a57babd3150b643c9aee8b6e5
jreleaserbot <jreleaser@kordamp.org>
2021-12-10 23:20:29 +0000

    jbang v0.84.1

--- jbang/tools/chocolateyinstall.ps1
+++ jbang/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package '{{projectName}}-{{projectVersion}}'
-$app_bat = Join-Path $app_home 'bin/{{distributionExecutable}}.cmd'
+$app_home = Join-Path $package 'jbang-0.84.1'
+$app_bat = Join-Path $app_home 'bin/jbang.cmd'
 
 Install-ChocolateyZipPackage `
-    -PackageName '{{distributionName}}' `
-    -Url '{{distributionUrl}}' `
-    -Checksum '{{distributionChecksumSha256}}' `
+    -PackageName 'jbang' `
+    -Url 'https://github.com/jbangdev/jbang/releases/download/v0.84.1/jbang-0.84.1.zip' `
+    -Checksum '63758146a2569677aebd7da0e9cb5d7d8c36837cbb697f6b99644d41b0b1d7a0' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
 
-Install-BinFile -Name '{{distributionExecutable}}' -Path $app_bat
+Install-BinFile -Name 'jbang' -Path $app_bat
--- jbang/tools/chocolateyuninstall.ps1
+++ jbang/tools/chocolateyuninstall.ps1
@@ -1,6 +1,6 @@
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package '{{projectName}}-{{projectVersion}}'
-$app_bat = Join-Path $app_home 'bin/{{distributionExecutable}}.cmd'
+$app_home = Join-Path $package 'jbang-0.84.1'
+$app_bat = Join-Path $app_home 'bin/jbang.cmd'
 
-Uninstall-BinFile -Name '{{distributionExecutable}}' -Path $app_bat
+Uninstall-BinFile -Name 'jbang' -Path $app_bat
