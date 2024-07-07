a95a9c99c9625ef22d0c0393bb28e31c1efa9743
jreleaserbot <jreleaser@kordamp.org>
2024-07-07 19:44:56 +0000

    jbang v0.117.0

--- jbang/tools/chocolateyuninstall.ps1
+++ jbang/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
-# Generated with JReleaser 1.9.0 at 2024-04-01T07:48:51.248947Z
+# Generated with JReleaser 1.9.0 at 2024-07-07T19:44:43.487219Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'jbang-0.116.0'
+$app_home = Join-Path $package 'jbang-0.117.0'
 $app_exe = Join-Path $app_home 'bin/jbang.cmd'
 
 Uninstall-BinFile -Name 'jbang' -Path $app_exe
