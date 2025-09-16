2a9a5259435e69ad30730bb2f5ae63062fa0a24a
jreleaserbot <jreleaser@kordamp.org>
2025-09-16 20:35:48 +0000

    jbang v0.131.0

--- jbang/tools/chocolateyinstall.ps1
+++ jbang/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
-# Generated with JReleaser 1.19.0 at 2025-09-10T00:50:20.073419Z
+# Generated with JReleaser 1.19.0 at 2025-09-16T20:34:55.787692Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'jbang-0.130.0'
+$app_home = Join-Path $package 'jbang-0.131.0'
 $app_exe = Join-Path $app_home 'bin/jbang.cmd'
 
 Install-ChocolateyZipPackage `
     -PackageName 'jbang' `
-    -Url 'https://github.com/jbangdev/jbang/releases/download/v0.130.0/jbang-0.130.0.zip' `
-    -Checksum '2cb544187e14165151d3b3b023dcb12816d5518b28fe953aa660f1d756805219' `
+    -Url 'https://github.com/jbangdev/jbang/releases/download/v0.131.0/jbang-0.131.0.zip' `
+    -Checksum 'b4ea18e8aea0ae62b80932b650d6c87b276412a84a04ff9d25a70da5dedd76a3' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
 
--- jbang/tools/chocolateyuninstall.ps1
+++ jbang/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
-# Generated with JReleaser 1.19.0 at 2025-09-10T00:50:20.073419Z
+# Generated with JReleaser 1.19.0 at 2025-09-16T20:34:55.787692Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'jbang-0.130.0'
+$app_home = Join-Path $package 'jbang-0.131.0'
 $app_exe = Join-Path $app_home 'bin/jbang.cmd'
 
 Uninstall-BinFile -Name 'jbang' -Path $app_exe
